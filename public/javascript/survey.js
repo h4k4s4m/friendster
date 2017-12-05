67eb62dddf05558aa63c8f6cb713ab07b3bcb817
Sahm Samarghandi <h4k4s4m@gmail.com>
2017-12-05 11:26:29 -0800

    more stuff

---
+++ public/javascript/survey.js
@@ -0,0 +1,15 @@
+$(document).on("click", "#submit", function (event) {
+    if ($("#name").val() == '' || $("#photo").val() == '') {
+        alert("Name and photo are required!");
+    } else {
+
+
+
+    }
+
+
+
+
+    alert($("#sel1").val());
+
+});
